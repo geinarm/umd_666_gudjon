97b98c8b5c79e889070aa9d38ab45d889712f3e6
Gudjon Magnusson <gmagnusson@fc-md.umd.edu>
2016-10-05 23:09:53 -0400

    Working on problem1. Implemented plot

--- assignment2/prob1.m
+++ assignment2/prob1.m
@@ -1,6 +1,38 @@
-j = 0:1000;
 
-t = j*(pi/1000);
-ut = 1./(1.1 - cos(t));
+u = @(t) (1/(1.1 - cos(t)));
+n = 10;
+np = 1000;
 
-plot(t, ut);
+j = 0:n;
+t = j'*pi/n;
+
+U = (1./(1.1 - cos(t)));
+Ue = [flip(U(3:end));U];
+te = [-flip(t(3:end));t];
+
+[a,b] = cossin_transform(Ue);
+K = (np-2*n)/2;
+ap = [a;zeros(K,1)];       % extend a and b vector with K zeros
+bp = [b;zeros(K,1)]; 
+Up = inv_cossin_transform(ap,bp);
+
+Utest = zeros(np, 1);
+for i = 1:np
+    sum = 0;
+    for k = 1:n+1
+       sum = sum + a(k)*cos(k*tp(i));
+    end
+    Utest(i) = sum;
+end
+
+% plot u(t)
+hold off;
+jp = 0:np-1;
+tp = jp*(pi/np);
+utp = 1./(1.1 - cos(tp));
+
+plot(tp, utp, '-'); hold on;
+plot(t, U, 'o');
+plot(te, Ue, 'o');
+plot(tp, Up, '-');
+plot(tp, Utest, '-');
